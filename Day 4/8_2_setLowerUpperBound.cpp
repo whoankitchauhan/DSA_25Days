dfb810399b76277886d73c2b774d5837f64b8170
Ankit Chauhan <138862610+whoankitchauhan@users.noreply.github.com>
2024-09-05 15:29:13 +0530

    Commit

--- Day 4/8_2_setLowerUpperBound.cpp
+++ Day 4/8_2_setLowerUpperBound.cpp
@@ -3,23 +3,30 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     // Create a set of integers
     set<int> mySet = {10, 20, 30, 40, 50};
 
     // Find lower bound of 25
     auto it_lower = mySet.lower_bound(25);
-    if (it_lower != mySet.end()) {
+    if (it_lower != mySet.end()) 
+    {
         cout << "Lower bound of 25: " << *it_lower << endl;
-    } else {
+    }
+    else
+    {
         cout << "Lower bound of 25 not found." << endl;
     }
 
     // Find upper bound of 25
     auto it_upper = mySet.upper_bound(25);
-    if (it_upper != mySet.end()) {
+    if (it_upper != mySet.end())
+    {
         cout << "Upper bound of 25: " << *it_upper << endl;
-    } else {
+    }
+    else
+    {
         cout << "Upper bound of 25 not found." << endl;
     }
 
@@ -33,4 +40,4 @@ int main() {
 // In simpler terms:
 
 // Lower bound finds the first element that is equal to or greater than the given value.
-// Upper bound finds the first element that is strictly greater than the given value.
+// Upper bound finds the first element that is strictly greater than the given value. 
